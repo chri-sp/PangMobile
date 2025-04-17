63a0804eb730d874e40e3755073e203eafe4e084
chri-sp <christiansipione@gmail.com>
2025-04-17 17:35:38 +0200

    Added movement animation

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -2,23 +2,45 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    private Rigidbody rb;
-    private float horizontal;
+    [SerializeField] private float m_Speed = 5f;
+    private Rigidbody m_Rb;
+    private float m_Horizontal;
+    
+    private Animator m_Animator;
+    private static int Animator_Walk = Animator.StringToHash("WALK");
+    private bool m_InRightDirection = true;
     
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        m_Rb = GetComponent<Rigidbody>();
+        m_Animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        m_Horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
+        
+        //check horizontal velocity to avoid walk animation against a wall
+        bool bWalk = Mathf.Abs(m_Rb.linearVelocity.x) > 0.1f;
+        
+        m_Animator.SetBool(Animator_Walk, bWalk);
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        m_Rb.linearVelocity = new Vector2(m_Horizontal * m_Speed, m_Rb.linearVelocity.y);
     }
     
+    private void Flip()
+    {
+        if ((m_Horizontal > 0f && !m_InRightDirection) || (m_Horizontal < 0f && m_InRightDirection))
+        {
+            m_InRightDirection = !m_InRightDirection;
+
+            Vector3 scale = transform.localScale;
+            scale.x *= -1f;
+            transform.localScale = scale;
+        }
+    }
 }
